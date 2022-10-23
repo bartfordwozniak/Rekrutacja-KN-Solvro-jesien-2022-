270f6cc614918861e757e00aba62250e19917a85
Bartford Wozniak <104282898+bartfordwozniak@users.noreply.github.com>
2022-10-24 00:17:03 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
-# This is a sample Python script.
-
-
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import tensorflow as tf
-import tensorflow_datasets as tfds
-import sklearn
-import jax, jaxlib
+import keras
+from tensorflow import keras
+from tensorflow.keras import layers
 
 
-#import os
-#os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'    #naprawa pewnego błędu, zapożyczone z stackoverflow
+
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'    #naprawa pewnego błędu, zapożyczone z stackoverflow
 
 
 
@@ -57,21 +55,51 @@ val_labels = np.load('data_sets/y_val.npy',
                        encoding = 'ASCII')
                         #encoding = 'OneHot')
 
-model = tf.keras.Sequential ([ tf.keras.layers.Flatten(input_shape=(300, 2)),  #(None, 300, 2)
-                             tf.keras.layers.Dense(4, activation="relu"),
-                             tf.keras.layers.Dense(4, activation="relu"),
-                             #tf.keras.layers.Dense(4, activation="relu"),
-                             tf.keras.layers.Dense(5, activation="softmax") ])
+model = keras.Sequential ([ keras.layers.Flatten(input_shape = (300, 2)),  #(None, 300, 2)
+                             keras.layers.Dense(8, activation = "relu"),
+                             keras.layers.Dense(16, activation = "selu"),
+                             keras.layers.Dense(8, activation = "relu"),
+                             keras.layers.Dense(4, activation = "relu"),
+                             # keras.layers.Dense(4, activation = "selu"),
+                             # keras.layers.Dense(4, activation = "relu"),
+                             keras.layers.Dense(5, activation = "softmax") ])
 
+# model = keras.Sequential()
+# model.add(layers.Dense(64, kernel_initializer='uniform', input_shape=(10,)))
+# model.add(layers.Activation('softmax'))
 
-model.compile(  loss = tf.keras.losses.SparseCategoricalCrossentropy(),
-                optimizer = tf.keras.optimizers.Adam(),
+model.compile(  loss = keras.losses.SparseCategoricalCrossentropy(),
+                #optimizer = keras.optimizers.Adam(),
                 metrics = "accuracy" )
 
-model.fit (train_data, train_labels, epochs=5)
-
-test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
-
-print('\nTest accuracy:', test_acc)
 
 
+optimalizer = keras.optimizers.Adam(learning_rate=0.01)
+model.compile (loss = 'categorical_crossentropy', optimizer = optimalizer, metrics=['Accuracy'])
+
+
+
+model.fit (train_data, train_labels, epochs=6)
+
+# test_loss, test_acc = model.evaluate(train_data,  train_labels, verbose=0)
+#
+# print("test loss: ", test_loss)
+# print("test acc: ", test_acc)
+
+print(model.metrics)
+#results = model.evaluate(val_data, val_labels, batch_size=64)
+# print("test loss, test acc:", results)
+#print(results)
+
+
+# results = model.evaluate(val_data, val_labels, batch_size=128)
+# #
+# print("test loss, test acc:", results[0], results[1])
+
+# score = model.evaluate(val_data, val_labels, verbose=0)
+# print('Test loss:', score[0])
+# print('\n')
+# print('Test accuracy:', score[1])
+
+
+#
