0f6052df582daecf723c3a3c7bfc6b6de239079b
Bartford Wozniak <104282898+bartfordwozniak@users.noreply.github.com>
2022-10-24 23:01:44 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,18 +1,6 @@
 import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import tensorflow as tf
-import keras
 from tensorflow import keras
-from tensorflow.keras import layers
-
-
-
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'    #naprawa pewnego błędu, zapożyczone z stackoverflow
-
-
-
+import tensorflow as tf
 #print(tf.__version__)
 
 #diffusion_types_names = ['attm', 'crtw', 'fbm', 'lw', 'sbm']
@@ -20,25 +8,21 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'    #naprawa pewnego błędu, zapożyczo
 train_data = np.load('data_sets/X_train.npy',
                        mmap_mode = None,
                        allow_pickle = False,
-                       #fix_imports = True,
+                       fix_imports = True,
                        encoding = 'ASCII')
-
-train_data = train_data/255
 
 train_labels = np.load('data_sets/y_train.npy',
                        mmap_mode = None,
                        allow_pickle = False,
-                       #fix_imports = True,
+                       fix_imports = True,
                        encoding = 'ASCII')
                         #encoding = 'OneHot')
 
 test_data = np.load('data_sets/X_test.npy',
                        mmap_mode = None,
                        allow_pickle = False,
-                       #fix_imports = True,
+                       fix_imports = True,
                        encoding = 'ASCII')
-
-test_data = test_data/255
 
 # test_labels = np.load('data_sets/y_test.npy',
 #                        mmap_mode = None,
@@ -49,63 +33,30 @@ test_data = test_data/255
 val_data = np.load('data_sets/X_val.npy',
                        mmap_mode = None,
                        allow_pickle = False,
-                       #fix_imports = True,
+                       fix_imports = True,
                        encoding = 'ASCII')
-
-val_data = val_data/255
 
 val_labels = np.load('data_sets/y_val.npy',
                        mmap_mode = None,
                        allow_pickle = False,
-                       #fix_imports = True,
+                       fix_imports = True,
                        encoding = 'ASCII')
                         #encoding = 'OneHot')
 
 model = keras.Sequential ([ keras.layers.Flatten(input_shape = (300, 2)),  #(None, 300, 2)
-                             keras.layers.Dense(8, activation = "relu"),
-                             keras.layers.Dense(16, activation = "selu"),
-                             keras.layers.Dense(8, activation = "relu"),
-                             keras.layers.Dense(4, activation = "relu"),
-                             # keras.layers.Dense(4, activation = "selu"),
-                             # keras.layers.Dense(4, activation = "relu"),
-                             keras.layers.Dense(5, activation = "softmax") ])
+                            keras.layers.Dense(128, activation = "relu"),
+                            keras.layers.Dense(256, activation = "selu"),
+                            keras.layers.Dense(128, activation = "sigmoid"),
+                            keras.layers.Dense(5, activation = "softmax") ])
 
-# model = keras.Sequential()
-# model.add(layers.Dense(64, kernel_initializer='uniform', input_shape=(10,)))
-# model.add(layers.Activation('softmax'))
+#optimalizer = keras.optimizers.Adam(learning_rate=0.01)
+optimalizer = keras.optimizers.SGD(learning_rate=0.005)
 
-model.compile(  loss = keras.losses.SparseCategoricalCrossentropy(),
-                #optimizer = keras.optimizers.Adam(),
-                metrics = "accuracy" )
-
-
-
-optimalizer = keras.optimizers.Adam(learning_rate=0.01)
 model.compile (loss = 'categorical_crossentropy', optimizer = optimalizer, metrics=['Accuracy'])
 
+# val_set = tf.data.Dataset.from_tensor_slices((val_data, val_labels))
+# val_set = val_set.batch(64)
 
-
-model.fit (train_data, train_labels, epochs=6)
-
-# test_loss, test_acc = model.evaluate(train_data,  train_labels, verbose=0)
-#
-# print("test loss: ", test_loss)
-# print("test acc: ", test_acc)
+model.fit (train_data, train_labels, batch_size=4, epochs=8) #,validation_data = val_set)
 
 print(model.metrics)
-#results = model.evaluate(val_data, val_labels, batch_size=64)
-# print("test loss, test acc:", results)
-#print(results)
-
-
-# results = model.evaluate(val_data, val_labels, batch_size=128)
-# #
-# print("test loss, test acc:", results[0], results[1])
-
-# score = model.evaluate(val_data, val_labels, verbose=0)
-# print('Test loss:', score[0])
-# print('\n')
-# print('Test accuracy:', score[1])
-
-
-#
