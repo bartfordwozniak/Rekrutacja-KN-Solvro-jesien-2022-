ba7d969f78c8d1c43b79882fdb41cac37a31f30a
Bartford Wozniak <104282898+bartfordwozniak@users.noreply.github.com>
2022-10-30 16:13:10 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,24 +1,34 @@
+#
+# ZADANIE REKRUTACYJNE DO KN SOLVRO
+#     Politechnika Wrocławska
+#       sem. zimowy 22/23
+#          Bartosz W.
+#           W12N, IMM
+#
+#--------------------------------------------------------------IMPORTY-------------------------------------------------
 import numpy as np
 from tensorflow import keras
-import tensorflow as tf
-#print(tf.__version__)
+from matplotlib import pyplot as plt
+import pandas as pd
+
+# from livelossplot import PlotLossesKeras
+
+#-------------------------------------------------------------POBIERANIE DANYCH--------------------------------------------------
 
 #diffusion_types_names = ['attm', 'crtw', 'fbm', 'lw', 'sbm']
 
-train_data = np.load('data_sets/X_train.npy',
+train_data = np.load ('data_sets/X_train.npy',
+                       mmap_mode = None,
+                       allow_pickle = False,
+                       fix_imports = True)
+
+train_labels = np.load ('data_sets/y_train.npy',
                        mmap_mode = None,
                        allow_pickle = False,
                        fix_imports = True,
                        encoding = 'ASCII')
 
-train_labels = np.load('data_sets/y_train.npy',
-                       mmap_mode = None,
-                       allow_pickle = False,
-                       fix_imports = True,
-                       encoding = 'ASCII')
-                        #encoding = 'OneHot')
-
-test_data = np.load('data_sets/X_test.npy',
+test_data = np.load ('data_sets/X_test.npy',
                        mmap_mode = None,
                        allow_pickle = False,
                        fix_imports = True,
@@ -30,33 +40,84 @@ test_data = np.load('data_sets/X_test.npy',
 #                        #fix_imports = True,
 #                        encoding = 'ASCII')
 
-val_data = np.load('data_sets/X_val.npy',
+val_data = np.load ('data_sets/X_val.npy',
                        mmap_mode = None,
                        allow_pickle = False,
                        fix_imports = True,
                        encoding = 'ASCII')
 
-val_labels = np.load('data_sets/y_val.npy',
+val_labels = np.load ('data_sets/y_val.npy',
                        mmap_mode = None,
                        allow_pickle = False,
                        fix_imports = True,
                        encoding = 'ASCII')
-                        #encoding = 'OneHot')
+
+#----------------------------------------------------ARCHITEKTURA-----------------------------------------------------
 
 model = keras.Sequential ([ keras.layers.Flatten(input_shape = (300, 2)),  #(None, 300, 2)
                             keras.layers.Dense(128, activation = "relu"),
                             keras.layers.Dense(256, activation = "selu"),
-                            keras.layers.Dense(128, activation = "sigmoid"),
+                            keras.layers.Dense(256, activation = "sigmoid"),
+                            keras.layers.Dense(128, activation = "relu"),
                             keras.layers.Dense(5, activation = "softmax") ])
 
 #optimalizer = keras.optimizers.Adam(learning_rate=0.01)
-optimalizer = keras.optimizers.SGD(learning_rate=0.005)
+optimalizer = keras.optimizers.SGD(learning_rate=0.01)
 
-model.compile (loss = 'categorical_crossentropy', optimizer = optimalizer, metrics=['Accuracy'])
+model.compile (loss = 'categorical_crossentropy',
+               optimizer = optimalizer,
+               metrics=['Accuracy'])
 
-# val_set = tf.data.Dataset.from_tensor_slices((val_data, val_labels))
-# val_set = val_set.batch(64)
+#-------------------------------------------------------TRENING-------------------------------------------------
 
-model.fit (train_data, train_labels, batch_size=4, epochs=8) #,validation_data = val_set)
+model.fit(train_data,
+          train_labels,
+          epochs = 3,
+          batch_size = 4,
+          #validation_data=(),
+          verbose=1,
+          shuffle=True,
+          #callbacks=callbacks_list
+          )
+
 
 print(model.metrics)
+
+
+
+#-----------------------------------------PRÓBY GENEROWANIA WYKRESÓW---------------------------------------------
+
+# plt.plot(history.history['Accuracy'])
+# plt.plot(history.history['val_acc'])
+# plt.title('model accuracy')
+# plt.ylabel('accuracy')
+# plt.xlabel('epoch')
+# plt.legend(['train', 'val'], loc='upper left')
+# plt.show()
+#
+# plt.plot(history.history['loss'])
+# plt.plot(history.history['val_loss'])
+# plt.title('model loss')
+# plt.ylabel('loss')
+# plt.xlabel('epoch')
+# plt.legend(['train', 'val'], loc='upper left')
+# plt.show()
+
+# OR (oba coś nie działają, overstack be like xd)
+
+# loss_train = history.history['train_loss']
+# loss_val = history.history['val_loss']
+# epochs = range(1,35)
+# plt.plot(epochs, loss_train, 'g', label='Training loss')
+# plt.plot(epochs, loss_val, 'b', label='validation loss')
+# plt.title('Training and Validation loss')
+# plt.xlabel('Epochs')
+# plt.ylabel('Loss')
+# plt.legend()
+# plt.show()
+
+#-------------------------------------------------------KLASYFIKACJA----------------------------
+
+dataframe = pd.write_csv(csv_file)
+col_names = ['index',
+             'class',]
